aeea117204e8a8cf7bb06d01f48b9f9851f4ed8e
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2021-08-12 11:19:52 +0200

    Basic PWM example implemented

--- src/main.c
+++ src/main.c
@@ -7,45 +7,79 @@
 #include <zephyr.h>
 #include <device.h>
 #include <devicetree.h>
-#include <drivers/gpio.h>
+#include <nrfx_pwm.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS   50
 
-/* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_ALIAS(led0)
+#define PWM_COUNTERTOP  100
 
-#if DT_NODE_HAS_STATUS(LED0_NODE, okay)
-#define LED0	DT_GPIO_LABEL(LED0_NODE, gpios)
-#define PIN	DT_GPIO_PIN(LED0_NODE, gpios)
-#define FLAGS	DT_GPIO_FLAGS(LED0_NODE, gpios)
-#else
-/* A build error here means your board isn't set up to blink an LED. */
-#error "Unsupported board: led0 devicetree alias is not defined"
-#define LED0	""
-#define PIN	0
-#define FLAGS	0
-#endif
+static nrfx_pwm_t my_pwm = NRFX_PWM_INSTANCE(1);
+
+static int pwm_init(void)
+{
+	static nrfx_pwm_config_t const config0 =
+    {
+        .output_pins =
+        {
+            30,						// channel 0
+            31,						// channel 1
+            NRFX_PWM_PIN_NOT_USED,	// channel 2
+            NRFX_PWM_PIN_NOT_USED	// channel 3
+        },
+        .irq_priority = 5,
+        .base_clock   = NRF_PWM_CLK_1MHz,
+        .count_mode   = NRF_PWM_MODE_UP,
+        .top_value    = PWM_COUNTERTOP,
+        .load_mode    = NRF_PWM_LOAD_INDIVIDUAL,
+        .step_mode    = NRF_PWM_STEP_AUTO
+    };
+    return (nrfx_pwm_init(&my_pwm, &config0, NULL, NULL) == NRFX_SUCCESS) ? 0 : -1;
+
+	// If PWM callbacks are to be used, remember to configure the interrupts correctly
+	//IRQ_DIRECT_CONNECT(PWM1_IRQn, 0, nrfx_pwm_1_irq_handler, 0);
+	//irq_enable(PWM1_IRQn);
+}
+
+static void pwm_set_duty_cycle(uint32_t dc1, uint32_t dc2)
+{
+	static bool pwm_running = false;
+
+    // This array cannot be allocated on stack (hence "static") and it must be in RAM 
+    static nrf_pwm_values_individual_t seq_values;
+
+	// Update the respective channels
+	seq_values.channel_0 = (dc1 <= PWM_COUNTERTOP) ? dc1 : PWM_COUNTERTOP;
+	seq_values.channel_1 = (dc2 <= PWM_COUNTERTOP) ? dc2 : PWM_COUNTERTOP;
+
+    nrf_pwm_sequence_t const seq =
+    {
+        .values.p_individual = &seq_values,
+        .length              = NRF_PWM_VALUES_LENGTH(seq_values),
+        .repeats             = 0,
+        .end_delay           = 0
+    };
+
+	if(!pwm_running){
+		pwm_running = true;
+		(void)nrfx_pwm_simple_playback(&my_pwm, &seq, 1000, NRFX_PWM_FLAG_LOOP);
+	}
+}
+
 
 void main(void)
 {
-	const struct device *dev;
-	bool led_is_on = true;
-	int ret;
-
-	dev = device_get_binding(LED0);
-	if (dev == NULL) {
+	if(pwm_init() != 0){
+		printk("Error initializing PWM\n");
 		return;
 	}
 
-	ret = gpio_pin_configure(dev, PIN, GPIO_OUTPUT_ACTIVE | FLAGS);
-	if (ret < 0) {
-		return;
-	}
+	printk("NRFX PMW example started\n");
 
 	while (1) {
-		gpio_pin_set(dev, PIN, (int)led_is_on);
-		led_is_on = !led_is_on;
+		static int counter = 0;
+		pwm_set_duty_cycle(counter, PWM_COUNTERTOP - counter);
+		counter = (counter + 1) % PWM_COUNTERTOP;
 		k_msleep(SLEEP_TIME_MS);
 	}
 }
